4894f2f6c5ed286d329e5bc4aa4cdfded92c6d42
romaintrotard <rtrotard@excilys.com>
2014-04-04 10:48:04 +0200

    Internationalization

--- Maven/computer-database/src/main/java/com/computerdatabase/validator/Validator.java
+++
@@ -1,9 +0,0 @@
-package com.computerdatabase.validator;
-
-public interface Validator {
-	
-	public boolean verifierDate(String date);
-	public boolean verifierName(String name);
-	public boolean comparerDate(String date1, String date2);
-	
-}
